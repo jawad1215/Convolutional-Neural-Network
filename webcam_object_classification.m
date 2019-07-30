cffc4aa188ed48558b89b9019d955c0b2e3fe012
jawad1215 <jawadyousafkhan@gmail.com>
2019-07-30 14:32:35 +0500

    Object Recognition Using Deep Learning

---
+++ webcam_object_classification.m
@@ -0,0 +1,20 @@
+% Copyright 2016 The MathWorks, Inc.
+close all 
+clc
+clear
+
+
+
+camera = webcam(1); % Connect to the camera
+nnet = alexnet;  % Load the neural net
+
+while true   
+    picture = camera.snapshot;              % Take a picture    
+    picture = imresize(picture,[227,227]);  % Resize the picture
+
+    label = classify(nnet, picture);        % Classify the picture
+       
+    image(picture);     % Show the picture
+    title(char(label)); % Show the label
+    drawnow;   
+end
